f6fc42fe8b1311d9a3b0bf49825fa1a04dd9de29
nicolasvalderrama689 <nicolasvalderrama689@gmail.com>
2024-10-15 16:07:08 -0500

    facturas

--- factura.go
+++ factura.go
@@ -14,42 +14,41 @@ import ( // Importación de bibliotecas
 
 // Estructura para almacenar los ítems
 type Item struct { // Define una estructura para representar un ítem en la factura
-	Descripcion    string  // Nombre o descripción del producto
-	Cantidad       int     // Cantidad del producto
-	PrecioUnitario float64 // Precio por unidad del producto 
+	Descripcion    string  
+	Cantidad       int     
+	PrecioUnitario float64 
 }
 
-var cliente string // Variable global para almacenar el nombre del cliente
+var cliente string 
 
 func main() { // Función principal del programa
 	var items []Item                  // Declara un slice para almacenar los productos agregados por el usuario
-	fmt.Print("Nombre del cliente: ") // Solicita el nombre del cliente
-	fmt.Scanln(&cliente)              // Lee el nombre del cliente ingresado y lo almacena en la variable 'cliente'
+	fmt.Print("Nombre del cliente: ") 
+	fmt.Scanln(&cliente)              // Lee el nombre del cliente ingresado y lo almacena en la var
 
 	reader := bufio.NewReader(os.Stdin) // Crea un nuevo lector para leer entradas del usuario lector eficiencia
 	for {                               // Bucle para agregar productos hasta que el usuario decida salir
-		fmt.Println("¿Desea añadir un producto a la factura? (s/n)") // Pregunta al usuario si quiere agregar un producto
-		confirm, _ := reader.ReadString('\n')                        // Lee la respuesta del usuario
-		confirm = strings.TrimSpace(confirm)                         // Elimina espacios en blanco adicionales de la respuesta
-		if confirm == "n" {                                          // Si el usuario responde "n", se rompe el bucle
+		fmt.Println("¿Desea añadir un producto a la factura? (s/n)") 
+		confirm, _ := reader.ReadString('\n')                        
+		confirm = strings.TrimSpace(confirm)                         
+		if confirm == "n" {                                       
 			break
 		}
 
 		// Leer descripción del producto
-		fmt.Print("Descripción del producto: ")      // Solicita la descripción del producto
-		descripcion, _ := reader.ReadString('\n')    // Lee la descripción del producto ingresada por el usuario
-		descripcion = strings.TrimSpace(descripcion) // Elimina espacios en blanco de la descripción
-
+		fmt.Print("Descripción del producto: ")      
+		descripcion, _ := reader.ReadString('\n')    
+		descripcion = strings.TrimSpace(descripcion)
 		// Leer cantidad
-		fmt.Print("Cantidad: ")                      // Solicita la cantidad del producto
-		cantidadStr, _ := reader.ReadString('\n')    // Lee la cantidad ingresada como cadena
-		cantidadStr = strings.TrimSpace(cantidadStr) // Elimina espacios en blanco de la cantidad
-		cantidad, _ := strconv.Atoi(cantidadStr)     // Convierte la cantidad de cadena a entero
+		fmt.Print("Cantidad: ")                      
+		cantidadStr, _ := reader.ReadString('\n')   
+		cantidadStr = strings.TrimSpace(cantidadStr) 
+		cantidad, _ := strconv.Atoi(cantidadStr)     
 
 		// Leer precio unitario
-		fmt.Print("Precio unitario: ")                         // Solicita el precio unitario del producto
-		precioStr, _ := reader.ReadString('\n')                // Lee el precio unitario ingresado como cadena
-		precioStr = strings.TrimSpace(precioStr)               // Elimina espacios en blanco del precio
+		fmt.Print("Precio unitario: ")                         
+		precioStr, _ := reader.ReadString('\n')              
+		precioStr = strings.TrimSpace(precioStr)               
 		precioUnitario, _ := strconv.ParseFloat(precioStr, 64) // Convierte el precio unitario de cadena a float64
 
 		// Agregar el producto a la lista de items
@@ -65,60 +64,60 @@ func main() { // Función principal del programa
 }
 
 func generarFacturaPDF(items []Item) { // Función para generar el PDF de la factura
-	fechaActual := time.Now().Format("02/01/2006 15:04:05") // Obtiene la fecha y hora actual en el formato especificado
+	fechaActual := time.Now().Format("02/01/2006 15:04:05") 
 
-	pdf := gofpdf.New("P", "mm", "A4", "") // Inicializa un nuevo documento PDF en formato A4
-	pdf.AddPage()                          // Añade una nueva página al PDF
-	pdf.SetFont("Arial", "", 14)           // Establece la fuente del texto en Arial, tamaño 14
+	pdf := gofpdf.New("P", "mm", "A4", "") // formato
+	pdf.AddPage()                         
+	pdf.SetFont("Arial", "", 14)        
 
 	// Encabezado del PDF
-	pdf.Cell(0, 10, "-----------------------------------------------------") // Dibuja una línea divisoria
-	pdf.Ln(10)                                                               // Salta una línea en el PDF
-	pdf.Cell(0, 10, "Factura")                                               // Título del documento
-	pdf.Ln(10)                                                               // Salta una línea
-	pdf.Cell(0, 10, "Numero de Factura: 12345")                              // Muestra un número de factura fijo
-	pdf.Ln(10)                                                               // Salta una línea
-	pdf.Cell(0, 10, fmt.Sprintf("Fecha: %s", fechaActual))                   // Agrega la fecha y hora actuales al PDF
-	pdf.Ln(10)                                                               // Salta una línea
-	pdf.Cell(0, 10, fmt.Sprintf("Cliente: %s ", cliente))                    // Muestra el nombre del cliente en el PDF
-	pdf.Ln(10)                                                               // Salta una línea
-	pdf.Cell(0, 10, "-----------------------------------------------------") // Dibuja otra línea divisoria
-	pdf.Ln(10)                                                               // Salta una línea
+	pdf.Cell(0, 10, "-----------------------------------------------------") 
+	pdf.Ln(10)                                                            
+	pdf.Cell(0, 10, "Factura")                                               
+	pdf.Ln(10)                                                          
+	pdf.Cell(0, 10, "Numero de Factura: 12345")                      
+	pdf.Ln(10)                                                             
+	pdf.Cell(0, 10, fmt.Sprintf("Fecha: %s", fechaActual))                   
+	pdf.Ln(10)                                                               
+	pdf.Cell(0, 10, fmt.Sprintf("Cliente: %s ", cliente))                    
+	pdf.Ln(10)                                                               
+	pdf.Cell(0, 10, "-----------------------------------------------------") 
+	pdf.Ln(10)                                                               
 
 	// Encabezado de la tabla de productos
-	pdf.Cell(40, 10, "Descripcion")                                          // Columna de descripción del producto
-	pdf.Cell(30, 10, "Cantidad")                                             // Columna de cantidad
-	pdf.Cell(40, 10, "Precio Unitario")                                      // Columna de precio unitario
-	pdf.Cell(40, 10, "Total")                                                // Columna de total por ítem
-	pdf.Ln(10)                                                               // Salta una línea
-	pdf.Cell(0, 10, "-----------------------------------------------------") // Dibuja una línea divisoria
-	pdf.Ln(10)                                                               // Salta una línea
+	pdf.Cell(40, 10, "Descripcion")                                         
+	pdf.Cell(30, 10, "Cantidad")                                        
+	pdf.Cell(40, 10, "Precio Unitario")                                    
+	pdf.Cell(40, 10, "Total")                                             
+	pdf.Ln(10)                                                              
+	pdf.Cell(0, 10, "-----------------------------------------------------") 
+	pdf.Ln(10)                                                               
 
 	// Agregar productos a la factura
 	var totalFinal float64       // Variable para calcular el total final de la factura d a e
 	for _, item := range items { // Recorre cada producto en la lista
-		total := float64(item.Cantidad) * item.PrecioUnitario      // Calcula el total por ítem
-		totalFinal += total                                        // Suma el total del ítem al total final
-		pdf.Cell(40, 10, item.Descripcion)                         // Muestra la descripción del producto
-		pdf.Cell(30, 10, strconv.Itoa(item.Cantidad))              // Muestra la cantidad del producto
+		total := float64(item.Cantidad) * item.PrecioUnitario      
+		totalFinal += total                                        
+		pdf.Cell(40, 10, item.Descripcion)                        
+		pdf.Cell(30, 10, strconv.Itoa(item.Cantidad))              
 		pdf.Cell(40, 10, fmt.Sprintf("%.2f", item.PrecioUnitario)) // Muestra el precio unitario del producto
 		pdf.Cell(40, 10, fmt.Sprintf("%.2f", total))               // Muestra el total por ítem
-		pdf.Ln(10)                                                 // Salta una línea
+		pdf.Ln(10)                                                
 	}
 
 	// Mostrar el total final de la factura
-	pdf.Cell(0, 10, "-----------------------------------------------------") // Dibuja una línea divisoria
-	pdf.Ln(10)                                                               // Salta una línea
-	pdf.Cell(40, 10, "Total Final")                                          // Muestra la etiqueta de total final
-	pdf.Cell(30, 10, "")                                                     // Espacio vacío
-	pdf.Cell(40, 10, "")                                                     // Espacio vacío
-	pdf.Cell(40, 10, fmt.Sprintf("%.2f", totalFinal))                        // Muestra el total final calculado
-	pdf.Ln(10)                                                               // Salta una línea
-	pdf.Cell(0, 10, "-----------------------------------------------------") // Dibuja una línea divisoria
+	pdf.Cell(0, 10, "-----------------------------------------------------") 
+	pdf.Ln(10)                                    
+	pdf.Cell(40, 10, "Total Final")                                         
+	pdf.Cell(30, 10, "")                                                     
+	pdf.Cell(40, 10, "")                                        
+	pdf.Cell(40, 10, fmt.Sprintf("%.2f", totalFinal))                     
+	pdf.Ln(10)                                                               
+	pdf.Cell(0, 10, "-----------------------------------------------------") 
 
 	// Guardar el PDF
-	err := pdf.OutputFileAndClose("factura.pdf") // Guarda el PDF con el nombre "factura.pdf"
-	if err != nil {                              // Verifica si hubo un error al guardar el PDF
-		log.Fatal(err) // Muestra un mensaje de error y detiene el programa si ocurre un problema
+	err := pdf.OutputFileAndClose("factura.pdf") 
+	if err != nil {                          
+		log.Fatal(err) 
 	}
 }
